4b29afc84cf1e5aa17842c0912a32a079d6c6553
Vitor Finotti <vfinotti@ncc.unesp.br>
2018-09-27 11:40:06 -0300

    Beautify top file

--- top/kc705/vhdl/cm0_softmc_top.vhd
+++ top/kc705/vhdl/cm0_softmc_top.vhd
@@ -30,55 +30,39 @@ library unisim;
 use unisim.vcomponents.all;
 
 entity cm0_softmc_top is
-  port (led0 : out std_logic;  -- dcm
-        led1 : out std_logic;  -- sleep
-        led2 : out std_logic;  -- lock
-        led3 : out std_logic;  -- detector
-        led4 : out std_logic;  -- reset
-        led5 : out std_logic;
-        led6 : out std_logic;
-        led7 : out std_logic;
-        sys_clk_p_i : in std_logic;
-        sys_clk_n_i : in std_logic);
+  port (
+    led0        : out std_logic;        -- dcm
+    led1        : out std_logic;        -- sleep
+    led2        : out std_logic;        -- lock
+    led3        : out std_logic;        -- detector
+    led4        : out std_logic;        -- reset
+    led5        : out std_logic;
+    led6        : out std_logic;
+    led7        : out std_logic;
+    sys_clk_p_i : in  std_logic;
+    sys_clk_n_i : in  std_logic);
 end cm0_softmc_top;
 
 architecture behavioral of cm0_softmc_top is
 
-  -- component memory
-  --   port (
-  --     clka  : in  std_logic;
-  --     rsta  : in  std_logic;
-  --     ena   : in  std_logic;
-  --     wea   : in  std_logic_vector(0 downto 0);
-  --     addra : in  std_logic_vector(8 downto 0);
-  --     dina  : in  std_logic_vector(31 downto 0);
-  --     douta : out std_logic_vector(31 downto 0));
-  -- end component;
-
   component rom_memory_blinking_led
     port (
-      clka : in std_logic;
-      rsta : in std_logic;
-      ena : in std_logic;
-      wea : in std_logic_vector(0 downto 0);
-      addra : in std_logic_vector(8 downto 0);
-      dina : in std_logic_vector(31 downto 0);
-      douta : out std_logic_vector(31 downto 0);
-      rsta_busy : out std_logic
-      );
+      clka      : in  std_logic;
+      rsta      : in  std_logic;
+      ena       : in  std_logic;
+      wea       : in  std_logic_vector(0 downto 0);
+      addra     : in  std_logic_vector(8 downto 0);
+      dina      : in  std_logic_vector(31 downto 0);
+      douta     : out std_logic_vector(31 downto 0);
+      rsta_busy : out std_logic);
   end component;
 
   component detectorbus is
-    port (clock : in std_logic;
-          databus : in std_logic_vector (31 downto 0);
-          detector : out std_logic);
+    port (
+      clock    : in  std_logic;
+      databus  : in  std_logic_vector (31 downto 0);
+      detector : out std_logic);
   end component;
-
-  -- component syncreset
-  --   port(
-  --     clock      : in  std_logic;
-  --     resetpulse : out std_logic);
-  -- end component;
 
 
   -- component systemclock
@@ -110,47 +94,47 @@ architecture behavioral of cm0_softmc_top is
 
 
 
-  signal rst_n : std_logic;
-  signal dummy : std_logic_vector(2 downto 0);
-  signal hrdata : std_logic_vector(31 downto 0);
-  signal hwdata : std_logic_vector(31 downto 0);
-  signal haddr : std_logic_vector(31 downto 0);
-  signal hburst : std_logic_vector(2 downto 0);
-  signal hprot : std_logic_vector(3 downto 0);
-  signal hsize : std_logic_vector(2 downto 0);
-  signal htrans : std_logic_vector(1 downto 0);
-  signal hwrite : std_logic_vector(0 downto 0);
+  signal rst_n      : std_logic;
+  signal dummy      : std_logic_vector(2 downto 0);
+  signal hrdata     : std_logic_vector(31 downto 0);
+  signal hwdata     : std_logic_vector(31 downto 0);
+  signal haddr      : std_logic_vector(31 downto 0);
+  signal hburst     : std_logic_vector(2 downto 0);
+  signal hprot      : std_logic_vector(3 downto 0);
+  signal hsize      : std_logic_vector(2 downto 0);
+  signal htrans     : std_logic_vector(1 downto 0);
+  signal hwrite     : std_logic_vector(0 downto 0);
   signal clk_200mhz : std_logic;
-  signal none : std_logic_vector(1 downto 0);
-  signal led_value : std_logic;
-  signal reset_rom : std_logic;
-  signal clk_50mhz : std_logic;
+  signal none       : std_logic_vector(1 downto 0);
+  signal led_value  : std_logic;
+  signal reset_rom  : std_logic;
+  signal clk_50mhz  : std_logic;
 
 begin
 
-  led3 <= led_value;
-  led4 <= rst_n;
-  led5 <= '1';
-  led6 <= '0';
-  led7 <= '1';
+  led3      <= led_value;
+  led4      <= rst_n;
+  led5      <= '1';
+  led6      <= '0';
+  led7      <= '1';
   reset_rom <= not rst_n;
 
   cpm_ibufgds_clk_gen : IBUFGDS
     generic map (
-      DIFF_TERM => false,  -- Differential Termination
-      IBUF_LOW_PWR => true,  -- Low power (TRUE) vs. performance (FALSE) setting for referenced I/O standards
-      IOSTANDARD => "DIFF_SSTL15"
+      DIFF_TERM    => false,            -- Differential Termination
+      IBUF_LOW_PWR => true,             -- Low power (TRUE) vs. performance (FALSE) setting for referenced I/O standards
+      IOSTANDARD   => "DIFF_SSTL15"
       )
     port map (
-      O => clk_200mhz,  -- Clock buffer output
-      I => sys_clk_p_i,  -- Diff_p clock buffer input (connect directly to top-level port)
-      IB => sys_clk_n_i  -- Diff_n clock buffer input (connect directly to top-level port)
+      O  => clk_200mhz,                 -- Clock buffer output
+      I  => sys_clk_p_i,                -- Diff_p clock buffer input (connect directly to top-level port)
+      IB => sys_clk_n_i                 -- Diff_n clock buffer input (connect directly to top-level port)
       );
 
   inst_detector : detectorbus
     port map (
-      clock => clk_50mhz,
-      databus => hrdata,
+      clock    => clk_50mhz,
+      databus  => hrdata,
       detector => led_value);
 
   -- inst_syncreset : syncreset
@@ -160,12 +144,12 @@ begin
 
   gc_single_reset_gen_1 : entity work.gc_single_reset_gen
     generic map (
-      g_out_reg_depth => 5,  -- delay for 5 clk cycles
-      g_rst_in_num => 1)  -- just 1 input
+      g_out_reg_depth => 5,             -- delay for 5 clk cycles
+      g_rst_in_num    => 1)             -- just 1 input
     port map (
-      clk_i => clk_50mhz,
+      clk_i             => clk_50mhz,
       rst_signals_n_a_i => "1",
-      rst_n_o => rst_n);
+      rst_n_o           => rst_n);
 
   -- inst_systemclock : systemclock
   --   port map(
@@ -177,29 +161,29 @@ begin
 
   sys_pll_1 : entity work.sys_pll
     generic map (
-      g_clkin_period => 5.000,  -- 200 MHz
-      g_divclk_divide => 1,
+      g_clkin_period   => 5.000,        -- 200 MHz
+      g_divclk_divide  => 1,
       g_clkbout_mult_f => 5,
-      g_clk0_divide_f => 20,  -- 50 MHz
-      g_clk1_divide => 20,  -- 50 MHz
-      g_clk2_divide => 20)  -- 50 MHz
+      g_clk0_divide_f  => 20,           -- 50 MHz
+      g_clk1_divide    => 20,           -- 50 MHz
+      g_clk2_divide    => 20)           -- 50 MHz
     port map (
-      rst_i => '0',
-      clk_i => clk_200mhz,
-      clk0_o => clk_50mhz,
-      clk1_o => open,
-      clk2_o => open,
+      rst_i    => '0',
+      clk_i    => clk_200mhz,
+      clk0_o   => clk_50mhz,
+      clk1_o   => open,
+      clk2_o   => open,
       locked_o => led0);
 
   inst_memory : rom_memory_blinking_led
     port map (
-      clka => clk_50mhz,
-      rsta => reset_rom,
-      ena => htrans(1),
-      wea => hwrite(0 downto 0),
-      addra => haddr(10 downto 2),
-      dina => hwdata(31 downto 0),
-      douta => hrdata(31 downto 0),
+      clka      => clk_50mhz,
+      rsta      => reset_rom,
+      ena       => htrans(1),
+      wea       => hwrite(0 downto 0),
+      addra     => haddr(10 downto 2),
+      dina      => hwdata(31 downto 0),
+      douta     => hrdata(31 downto 0),
       rsta_busy => open);
 
   cortex_m0_1 : entity work.cortex_m0_wrapper
