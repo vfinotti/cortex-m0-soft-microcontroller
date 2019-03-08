6abd07b0ecffa4e696567004f176ff04a1e8464b
Vitor Finotti <vfinotti@ncc.unesp.br>
2019-03-08 19:03:38 -0300

    Adding timer slave to DMA manifest.py

--- top/kc705_dma/verilog/Manifest.py
+++ top/kc705_dma/verilog/Manifest.py
@@ -7,6 +7,7 @@ files = [
     "../../../ip_cores/roa_logic/ahb3lite_pkg/rtl/verilog/ahb3lite_pkg.sv",
     "../../../ip_cores/roa_logic/ahb3lite_memory/rtl/verilog/ahb3lite_sram1rw.sv",
     "../../../ip_cores/roa_logic/memory/rtl/verilog/rl_ram_1r1w.sv",
+    "../../../ip_cores/roa_logic/ahb3lite_timer/rtl/verilog/ahb3lite_timer.sv",
     "../../../ip_cores/vhdl-extras/rtl/extras/cordic.vhdl",
     "../../../ip_cores/vhdl-extras/rtl/extras/pipelining.vhdl"
 ]
