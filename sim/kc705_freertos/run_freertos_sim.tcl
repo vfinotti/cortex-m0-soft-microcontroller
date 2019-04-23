5ffc0399c9b4b1fddbedb1088ea137e7597f2f61
Vitor Finotti <vfinotti@ncc.unesp.br>
2019-04-23 14:03:47 -0300

    Increasing simulation time to 2000us

--- sim/kc705_freertos/run_freertos_sim.tcl
+++ sim/kc705_freertos/run_freertos_sim.tcl
@@ -1,4 +1,4 @@
 restart
 add_force {/cm0_freertos_top/clk_200mhz} -radix hex {0 0ns} {1 2500ps} -repeat_every 5000ps
 add_force {/cm0_freertos_top/push_button0_i} -radix hex {1 0ns} {0 10000000ps} -repeat_every 10000000000ps
-run 1200us
+run 2000us
